9ca0574554c0393ed7bc5277d186cef0610c7ed1
Georg Dangl <georg@georgdangl.de>
2017-01-19 20:51:16 +0100

    Update spacker.exe and build.bat to emit shaders in TypeScript

--- Xbim.WeXplorer/build.bat
+++ Xbim.WeXplorer/build.bat
@@ -1,6 +1,8 @@
 rem Generate JS file containing all shaders as a JS strings. Shaders are authored in *.c files which makes it easier for development and it has partially right syntax highlighting
 Utilities\spacker.exe Viewer xbim-shaders.debug.js -min
+Utilities\spacker.exe Viewer xbim-shaders.ts -typescript -min
 Utilities\spacker.exe Plugins\NavigationCube xbim-navigation-cube-shaders.debug.js -min
+Utilities\spacker.exe Plugins\NavigationCube xbim-navigation-cube-shaders.ts -typescript -min
 
 rem Create debug bundle. It contains all JS source code in one file including all comments. This is easy to reference and still possible to debug
 type Resources\xbim-disclaimer.txt > Build\xbim-viewer.debug.bundle.js
