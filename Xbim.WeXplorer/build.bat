9ca2b1ba4cd16ecd584a0df4fb03bb75f56dd883
Martin Cerny <martin.cerny@xbim.cz>
2017-02-10 17:26:14 +0100

    Doc generation made part of the build.

--- Xbim.WeXplorer/build.bat
+++ Xbim.WeXplorer/build.bat
@@ -1,11 +1,13 @@
 rem Generate JS file containing all shaders as a TypeScript strings. Shaders are authored in *.c files which makes it easier for development and it has partially right syntax highlighting
-Utilities\spacker.exe Viewer xbim-shaders.ts -typescript -min
-Utilities\spacker.exe Plugins\NavigationCube xbim-navigation-cube-shaders.ts -typescript -min -variable:xCubeShaders
+Utilities\spacker.exe Viewer xbim-shaders.ts -typescript -min && ^
+Utilities\spacker.exe Plugins\NavigationCube xbim-navigation-cube-shaders.ts -typescript -min -variable:xCubeShaders && ^
 
 rem Generate all bundles of the complete project and of independent parts
-npm run webpack
+npm run webpack && ^
 
+rem Generate fresh documentation
+npm run jsdoc && ^
 
 rem Copy new built files to the sample project to make sure it's running the with the latest source
-xcopy /y Build\xbim.bundle.js Resources\doctemplate\static\scripts
-xcopy /y Build\xbim.bundle.js.map Resources\doctemplate\static\scripts
+xcopy /y Build\xbim.bundle.js Resources\doctemplate\static\scripts\
+xcopy /y Build\xbim.bundle.js.map Resources\doctemplate\static\scripts\
