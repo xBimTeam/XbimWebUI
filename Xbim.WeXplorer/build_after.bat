e0e6a693407da919c602599790e25b6bcb881d03
Martin Cerny <martin.cerny@xbim.cz>
2017-04-25 15:58:41 +0200

    Nuspec++

--- Xbim.WeXplorer/build_after.bat
+++ Xbim.WeXplorer/build_after.bat
@@ -1,12 +1,12 @@
-Utilities\TypingsBundler.exe Build
+rem Utilities\TypingsBundler.exe Build
 
 rem Generate fresh documentation
-npm run jsdoc && ^
+rem npm run jsdoc && ^
 
 rem Build packages
-SET NODE_ENV=development&& webpack -d && ^
-SET NODE_ENV=production&& webpack -p && ^
+rem SET NODE_ENV=development&& webpack -d && ^
+rem SET NODE_ENV=production&& webpack -p && ^
 
 rem Copy new built files to the sample project to make sure it's running the with the latest source
-xcopy /y Build\xbim-viewer.js Resources\doctemplate\static\scripts\
-xcopy /y Build\xbim-browser.js Resources\doctemplate\static\scripts\
+rem xcopy /y Build\xbim-viewer.js Resources\doctemplate\static\scripts\
+rem xcopy /y Build\xbim-browser.js Resources\doctemplate\static\scripts\
