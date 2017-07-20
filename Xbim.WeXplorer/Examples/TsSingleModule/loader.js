7f32eef78d75b020ff41daf7e2a606b7d3cfaa4f
Martin Cerny <martin.cerny@xbim.net>
2017-07-20 13:14:49 +0200

    Missed file because of too constrained .gitignore

---
+++ Xbim.WeXplorer/Examples/TsSingleModule/loader.js
@@ -0,0 +1,13 @@
+﻿SystemJS.config({
+    packages: {
+        // meaning [baseURL]/local/package when no other rules are present
+        // path is normalized using map and paths configuration
+        '/': {
+            main: 'index.js',
+            format: 'cjs',
+            defaultExtension: 'js'
+        }
+    }
+
+});
+SystemJS.import('index.js');
