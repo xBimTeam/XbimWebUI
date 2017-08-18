11d85a28773a7422ac751f2c244e1ddf5d8c047f
Martin Cerny <martin.cerny@xbim.net>
2017-08-18 11:38:43 +0200

    browser removed from after-build script

--- Xbim.WeXplorer/build_after.bat
+++ Xbim.WeXplorer/build_after.bat
@@ -7,4 +7,3 @@ SET NODE_ENV=production&& webpack -p && ^
 
 rem Copy new built files to the sample project to make sure it's running the with the latest source
 xcopy /y Build\xbim-viewer.js Resources\doctemplate\static\scripts\
-xcopy /y Build\xbim-browser.js Resources\doctemplate\static\scripts\
