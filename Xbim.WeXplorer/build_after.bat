c030f810c5e3ed7ed097efa17bba13e4bc6c7232
Martin Cerny <martin.cerny@xbim.net>
2018-03-05 15:10:09 +0100

    Project restructuring. Dropping Visual Studio dependencies.

--- Xbim.WeXplorer/build_after.bat
+++
@@ -1,9 +0,0 @@
-rem Generate fresh documentation
-npm run jsdoc && ^
-
-rem Build packages
-SET NODE_ENV=development&& webpack -d && ^
-SET NODE_ENV=production&& webpack -p && ^
-
-rem Copy new built files to the sample project to make sure it's running the with the latest source
-xcopy /y Build\xbim-viewer.js Resources\doctemplate\static\scripts\
