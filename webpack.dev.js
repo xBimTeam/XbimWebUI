f871d4374aa4205d8268e10b9cc361f940fe737a
Martin Cerny <martin.cerny@xbim.net>
2018-03-07 13:25:00 +0100

    concurrent watch for dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
-
 module.exports = merge(common, {
     mode: "development",
     devtool: 'inline-source-map',
--- webpack.worker.js
+++ webpack.worker.js
@@ -31,6 +31,6 @@ module.exports = {
         ]
     },
     resolve: {
-        extensions: ['.ts', '.js', '.c']
+        extensions: ['.ts', '.js']
     }
 };
