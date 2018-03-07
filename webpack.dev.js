4eb48098de01aa69a3d92545921aed637f8da7a0
Martin Cerny <martin.cerny@xbim.net>
2018-03-07 09:01:51 +0100

    dev server public path adjusted to emulate build path

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
     devServer: {
         contentBase: ".",
         host: "localhost",
-        publicPath: "/build/",
+        publicPath: "/dist/",
         port: 9000
     },
 });
