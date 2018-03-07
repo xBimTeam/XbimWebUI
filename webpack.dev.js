07d655a8481abc3f67d5b4637ac0f170994d4aff
Martin Cerny <martin.cerny@xbim.net>
2018-03-07 10:34:39 +0100

    Hot Module Replacement

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,24 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
+
 
 module.exports = merge(common, {
     mode: "development",
     devtool: 'inline-source-map',
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
         contentBase: ".",
         host: "localhost",
         publicPath: "/dist/",
-        port: 9000
+        port: 9000,
+        hot: true,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
     },
 });
