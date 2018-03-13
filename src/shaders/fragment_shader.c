f18fd7bb7e619f8110d411d1bab793fc3ba5c693
Martin Cerny <martin.cerny@xbim.net>
2018-03-13 14:27:21 +0100

    GLSL ES 100 shaders upgraded to use phong shading.

--- src/shaders/fragment_shader.c
+++ src/shaders/fragment_shader.c
@@ -5,11 +5,15 @@ uniform vec4 uClippingPlaneB;
 uniform bool uClippingA;
 uniform bool uClippingB;
 
+// Light position
+uniform vec3 uLight;
 
-varying vec4 vFrontColor;
-varying vec4 vBackColor;
+varying vec4 vColor;
 //position in real world. This is used for clipping.
 varying vec3 vPosition;
+// normal in real world for Phong shading
+varying vec3 vNormal;
+
 //state passed to fragment shader
 varying float vDiscard;
 
@@ -27,7 +31,6 @@ void main(void) {
 		if (distance < 0.0){
 			discard;
 		}
-		
 	}
 
 	//test if clipping plane is defined
@@ -43,6 +46,43 @@ void main(void) {
 
 	}
 	
+	// it is colour coding. Don't to shading and lighting or anything else
+	if (length(vNormal) == 0.0) {
+		gl_FragColor = vColor;
+		return;
+	}
+
 	//fix wrong normals (supposing the orientation of vertices is correct but normals are flipped)
-	gl_FragColor = gl_FrontFacing ? vFrontColor : vBackColor;
+	vec3 normalInterp = gl_FrontFacing ? vNormal : -vNormal;
+
+	// Phong shading (http://multivis.net/lecture/phong.html)
+	float Ka = 1.0;           // Ambient reflection coefficient [0.0,1.0]
+	float Kd = 1.0;           // Diffuse reflection coefficient [0.0,1.0]
+	float Ks = 0.3;           // Specular reflection coefficient [0.0,1.0], default 1.0
+	float shininessVal = 2.0; // Shininess [1.0,128.0], default 4
+
+	vec3 ambientColor = vColor.rgb * 0.2; // default (0.0, 0.0, 0.0)
+	vec3 diffuseColor = vColor.rgb;
+	vec3 specularColor = vec3(1.0, 1.0, 1.0);
+
+	vec3 N = normalize(normalInterp);
+  	vec3 L = normalize(uLight - vPosition);
+
+  	// Lambert's cosine law
+  	float lambertian = max(dot(N, L), 0.0);
+
+  	float specular = 0.0;
+
+  	if(lambertian > 0.0) {
+  	  vec3 R = reflect(-L, N);        // Reflected light vector
+  	  vec3 V = normalize(-vPosition); // Vector to viewer
+
+  	  // Compute the specular term
+  	  float specAngle = max(dot(R, V), 0.0);
+  	  specular = pow(specAngle, shininessVal);
+  	}
+
+  	gl_FragColor = vec4(Ka * ambientColor +
+                      Kd * lambertian * diffuseColor +
+                      Ks * specular * specularColor, vColor.a);
 }
--- src/shaders/vertex_shader.c
+++ src/shaders/vertex_shader.c
@@ -9,10 +9,6 @@ attribute highp vec2 aNormal;
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 
-//Lights
-uniform vec4 ulightA;
-uniform vec4 ulightB;
-
 //Highlighting colour
 uniform vec4 uHighlightColour;
 
@@ -46,12 +42,13 @@ uniform highp float uStyleTextureSize;
 uniform highp sampler2D uStateStyleSampler;
 
 //colour to go to fragment shader
-varying vec4 vFrontColor;
-varying vec4 vBackColor;
+varying vec4 vColor;
 //varying position used for clipping in fragment shader
 varying vec3 vPosition;
+// varying normal used for shading
+varying vec3 vNormal;
 //state passed to fragment shader
-varying float vDiscard;
+varying mediump float vDiscard;
 
 const float PI = 3.1415926535897932384626433832795;
 
@@ -175,8 +172,8 @@ void main(void) {
 			))
 	{
 		vDiscard = 1.0;
-		vFrontColor = vec4(0.0, 0.0, 0.0, 0.0);
-		vBackColor = vec4(0.0, 0.0, 0.0, 0.0);
+		vColor = vec4(0.0, 0.0, 0.0, 0.0);
+		vNormal = vec3(0.0, 0.0, 0.0);
 		vPosition = vec3(0.0, 0.0, 0.0);
 		gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
 		return;
@@ -186,42 +183,23 @@ void main(void) {
 	float tIndex = aTransformationIndex * 4.0;
 	float tSize = uMatrixTextureSize;
 	mat4 transform = getTransform();
-	vec3 vertex = getVertexPosition(transform);
-	vec3 normal = getNormal(transform);
-	vec3 backNormal = normal * -1.0;
+	vPosition = getVertexPosition(transform);
+	vNormal = getNormal(transform);
 
 	//product ID colour coding
 	if (uColorCoding == -2) {
 		float id = floor(aProduct + 0.5);
-		vec4 idColor = getIdColor(id);
-		vFrontColor = idColor;
-		vBackColor = idColor;
-		//model ID colour coding
+		vColor = getIdColor(id);
+		vNormal = vec3(0.0, 0.0, 0.0);
 	}
+	//model ID colour coding
 	else if (uColorCoding >= 0) {
 		float id = float(uColorCoding);
-		vec4 idColor = getIdColor(id);
-		vFrontColor = idColor;
-		vBackColor = idColor;
-		// rendering
+		vColor = getIdColor(id);
+		vNormal = vec3(0.0, 0.0, 0.0);
 	}
+	// rendering
 	else {
-		// ulightA[3] represents intensity of the light
-		float lightAIntensity = ulightA[3];
-		vec3 lightADirection = normalize(ulightA.xyz - vertex);
-		float lightBIntensity = ulightB[3];
-		vec3 lightBDirection = normalize(ulightB.xyz - vertex);
-
-		// Light weighting
-		float lightWeightA = max(dot(normal, lightADirection) * lightAIntensity, 0.0);
-		float lightWeightB = max(dot(normal, lightBDirection) * lightBIntensity, 0.0);
-		float backLightWeightA = max(dot(backNormal, lightADirection) * lightAIntensity, 0.0);
-		float backLightWeightB = max(dot(backNormal, lightBDirection) * lightBIntensity, 0.0);
-
-		// minimal constant value is for ambient light
-		float lightWeighting = lightWeightA + lightWeightB + 0.4;
-		float backLightWeighting = backLightWeightA + backLightWeightB + 0.4;
-
 		// get base color or set highlighted colour
 		vec4 baseColor = vec4(1.0, 1.0, 1.0, 1.0);
 		// highlighted takes precedense
@@ -230,30 +208,31 @@ void main(void) {
 			// x-ray mode 
 		}
 		else if (uRenderingMode == 2 || uRenderingMode == 3) {
-			if (state == 252) { //x-ray visible
+			//x-ray visible
+			if (state == 252) { 
 				baseColor = getColor();
 			}
+			//x-ray semitransparent light blue colour
 			else {
-				baseColor = vec4(0.0, 0.0, 0.3, 0.5); //x-ray semitransparent light blue colour
+				baseColor = vec4(0.0, 0.0, 0.3, 0.5); 
 			}
-			// normal colour (or overriding)
 		}
 		else {
+			// normal colour (or overriding)
 			baseColor = getColor();
 		}
 
 		//offset semitransparent triangles
 		if (baseColor.a < 0.98 && uRenderingMode == 0)
 		{
-			vec3 trans = -0.002 * uMeter * normalize(normal);
-			vertex = vertex + trans;
+			vec3 trans = -0.002 * uMeter * normalize(vNormal);
+			vPosition = vPosition + trans;
 		}
 
-		//transform colour to simulate lighting
-		//preserve original alpha channel
-		vFrontColor = vec4(baseColor.rgb * lightWeighting, baseColor.a);
-		vBackColor = vec4(baseColor.rgb * backLightWeighting, baseColor.a);
+		// pass colour to fragment thader
+		vColor = baseColor;
 	}
-	vPosition = vertex;
-	gl_Position = uPMatrix * uMVMatrix * vec4(vertex, 1.0);
+
+	// transform to GL space
+	gl_Position = uPMatrix * uMVMatrix * vec4(vPosition, 1.0);
 }
