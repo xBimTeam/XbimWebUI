8342e13bb19489fb2d23a743ad30bf3826cf00e8
Martin Cerny <martin.cerny@xbim.net>
2018-07-02 09:45:22 +0200

    Fixed depth mask for x-ray rendering

--- src/model-handle.ts
+++ src/model-handle.ts
@@ -354,7 +354,6 @@ export class ModelHandle {
 
         if (mode == null) {
             // make sure depth testing is on for general rendering
-            gl.depthMask(true);
             if (maps == null) {
                 gl.drawArrays(gl.TRIANGLES, 0, this._numberOfIndices);
             } else {
@@ -369,7 +368,6 @@ export class ModelHandle {
 
         if (mode === DrawMode.SOLID && this._model.transparentIndex > 0) {
             // make sure depth testing is on for solid rendering
-            gl.depthMask(true);
             if (maps == null) {
                 gl.drawArrays(gl.TRIANGLES, 0, this._model.transparentIndex);
             } else {
