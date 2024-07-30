97faf07d26bd2f78bf587c4031defb0eae940a5d
Ibrahim Saad <ibrahim.saad@xbim.net>
2024-07-30 17:31:58 +0200

    Added documentations for the data visualization plugins

--- src/plugins/DataVisualization/Heatmap/heatmap-source.ts
+++ src/plugins/DataVisualization/Heatmap/heatmap-source.ts
@@ -1,3 +1,7 @@
+/**
+ * The HeatmapSource class represents the producers of values for the heatmap.
+ * Sources are responsible for providing and feeding the data to the IHeatmapChannel instances.
+ */
 export class HeatmapSource {
     private _id: string;
     private _modelId: number;
@@ -5,6 +9,15 @@ export class HeatmapSource {
     private _channelId: string;
     private _value: any;
 
+    /**
+     * Creates an instance of HeatmapSource.
+     * 
+     * @param {string} id - A unique identifier for the heatmap source.
+     * @param {number} modelId - The model ID associated with the source.
+     * @param {number} productId - The product ID associated with the source.
+     * @param {string} channelId - The channel ID associated with the source.
+     * @param {any} value - The value produced by the source.
+     */
     constructor(id: string, modelId: number, productId: number, channelId: string, value: any) {
         this._id = id;
         this._modelId = modelId;
@@ -13,26 +26,50 @@ export class HeatmapSource {
         this._value = value;
     }
 
+    /**
+     * Gets the unique identifier for the heatmap source.
+     * @returns {string} The source ID.
+     */
     public get id(): string {
         return this._id;
     }
 
+    /**
+     * Gets the model ID associated with the source.
+     * @returns {number} The model ID.
+     */
     public get modelId(): number {
         return this._modelId;
     }
 
+    /**
+     * Gets the product ID associated with the source.
+     * @returns {number} The product ID.
+     */
     public get productId(): number {
         return this._productId;
     }
 
+    /**
+     * Gets the channel ID associated with the source.
+     * @returns {string} The channel ID.
+     */
     public get channelId(): string {
         return this._channelId;
     }
 
+    /**
+     * Gets the value produced by the source.
+     * @returns {any} The value.
+     */
     public get value(): any {
         return this._value;
     }
 
+    /**
+     * Sets the value produced by the source.
+     * @param {any} value - The new value.
+     */
     public set value(value: any) {
         this._value = value;
     }
--- src/plugins/DataVisualization/Icons/icon.ts
+++ src/plugins/DataVisualization/Icons/icon.ts
@@ -1,3 +1,10 @@
+import { Icons } from './icons'
+
+/**
+ * Represents an icon that can be rendered using the Icons plugin
+ * 
+ * @see Icons
+ */
 export class Icon {
     private _modelId: number;
     private _productId: number;
@@ -17,14 +24,24 @@ export class Icon {
      * @param {number} modelId - The model ID associated with the icon.
      * @param {number} productId - The product ID associated with the icon.
      * @param {string} imageData - Base64 encoded image data for the icon.
-     * @param {Float32Array} [location=null] - The XYZ coordinates for the icon location. If not provided, the centroid of the product bounding box is used.
+     * @param {Float32Array | null} [location=null] - The XYZ coordinates for the icon location. If not provided, the centroid of the product bounding box is used.
      * @param {number | null} [width=null] - The width of the icon. If null, default width is used.
      * @param {number | null} [height=null] - The height of the icon. If null, default height is used.
      * @param {() => void} [onIconSelected=null] - Callback function to be executed when the icon is selected.
      * @example
-     * const icon = new Icon('Sample Icon', 'This is a sample icon.', 1, 101, 'imageDataString', 100, 100, () => console.log('Icon selected'));
+     * const icon = new Icon('Sample Icon', 'This is a sample icon.', 1, 101, 'imageDataString', new Float32Array([0, 0, 0]), 100, 100, () => console.log('Icon selected'));
      */
-    constructor(name: string, description: string, modelId: number, productId: number, imageData: string, location: Float32Array | null = null, width: number | null = null, height: number | null = null, onIconSelected: () => void = null) {
+    constructor(
+        name: string,
+        description: string,
+        modelId: number,
+        productId: number,
+        imageData: string,
+        location: Float32Array | null = null,
+        width: number | null = null,
+        height: number | null = null,
+        onIconSelected: () => void = null
+    ) {
         this._modelId = modelId;
         this._productId = productId;
         this._imageData = imageData;
@@ -36,62 +53,122 @@ export class Icon {
         this._onIconSelected = onIconSelected;
     }
 
+    /**
+     * Gets the model ID associated with the icon.
+     * @returns {number} The model ID.
+     */
     public get modelId(): number {
         return this._modelId;
     }
 
+    /**
+     * Gets the product ID associated with the icon.
+     * @returns {number} The product ID.
+     */
     public get productId(): number {
         return this._productId;
     }
 
+    /**
+     * Gets the location of the icon.
+     * @returns {Float32Array} The XYZ coordinates of the icon.
+     */
     public get location(): Float32Array {
         return this._location;
     }
 
+    /**
+     * Sets the location of the icon.
+     * @param {Float32Array} value - The new XYZ coordinates for the icon.
+     */
     public set location(value: Float32Array) {
          this._location = value;
     }
 
+    /**
+     * Gets the Base64 encoded image data of the icon.
+     * @returns {string} The Base64 encoded image data.
+     */
     public get imageData(): string {
         return this._imageData;
     }
 
+    /**
+     * Sets the Base64 encoded image data of the icon.
+     * @param {string} value - The new Base64 encoded image data.
+     */
     public set imageData(value: string) {
         this._imageData = value;
     }
 
+    /**
+     * Gets the name of the icon.
+     * @returns {string} The name of the icon.
+     */
     public get name(): string {
         return this._name;
     }
 
+    /**
+     * Sets the name of the icon.
+     * @param {string} value - The new name of the icon.
+     */
     public set name(value: string) {
         this._name = value;
     }
 
+    /**
+     * Gets the description of the icon.
+     * @returns {string} The description of the icon.
+     */
     public get description(): string {
         return this._description;
     }
 
+    /**
+     * Sets the description of the icon.
+     * @param {string} value - The new description of the icon.
+     */
     public set description(value: string) {
         this._description = value;
     }
 
+    /**
+     * Gets the width of the icon.
+     * @returns {number} The width of the icon.
+     */
     public get width(): number {
         return this._width;
     }
 
+    /**
+     * Sets the width of the icon.
+     * @param {number} value - The new width of the icon.
+     */
     public set width(value: number) {
         this._width = value;
     }
 
+    /**
+     * Gets the height of the icon.
+     * @returns {number} The height of the icon.
+     */
     public get height(): number {
         return this._height;
     }
 
+    /**
+     * Sets the height of the icon.
+     * @param {number} value - The new height of the icon.
+     */
     public set height(value: number) {
         this._height = value;
     }
 
+    /**
+     * Gets the callback function to be executed when the icon is selected.
+     * @returns {() => void} The callback function.
+     */
     public get onIconSelected(): () => void {
         return this._onIconSelected;
     }
